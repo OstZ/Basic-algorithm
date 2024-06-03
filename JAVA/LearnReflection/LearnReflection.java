1e69ad2d1fc631ecc87d74e33b98b60b793d00df
OstZ <zsm1597183514@outlook.com>
2024-06-03 17:18:25 +0800

    Reflection

--- JAVA/LearnReflection/LearnReflection.java
+++ JAVA/LearnReflection/LearnReflection.java
@@ -1,5 +1,7 @@
 package JAVA.LearnReflection;
 
+import org.junit.Test;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -17,13 +19,14 @@ public class LearnReflection {
         System.out.println(cls1.getName());
 
         //fields
-        Field[] fields = cls.getDeclaredFields();
-        System.out.println(fields);
+        Field field = cls.getDeclaredField("name");
+        System.out.println("modifier: " +field.getModifiers());
+        System.out.println(field);
 
         //change name
-        Field field = cls.getField("name");
+//        Field field = cls.getField("name");
 //        System.out.println(field.getName());
-        field.set(obj, "mike");
+//        field.set(obj, "mike");
         System.out.println(field.get(obj));
 
         //use method
@@ -31,7 +34,29 @@ public class LearnReflection {
         Method mtd = cls.getMethod("run", clstmp);
         mtd.invoke(obj, 23);
 
+        //class for class
+        Class clsnow = Class.forName("java.lang.Class");
+        System.out.println("clsnow: " + clsnow.getName());
 
+        //static method
+        Method mtds = cls.getMethod("bark");
+        mtds.invoke(null);
 
+        //
+        hw02();
+
+    }
+    public static void hw02() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        //get class obj of file
+        Class filecls = Class.forName("java.io.File");
+        //print constructors
+        Constructor<?>[] csts = filecls.getConstructors();
+        for (Constructor<?> cststmp : csts) {
+            System.out.println(cststmp);
+        }
+        //create file obj
+        Object objfile = filecls.getConstructor(String.class).newInstance("/home/zsm/CS/books/DSA/code/JAVA/LearnReflection/re.txt");
+        Method filemtd = filecls.getMethod("createNewFile");
+        filemtd.invoke(objfile);
     }
 }
